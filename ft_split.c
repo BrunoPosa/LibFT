cc049abfb3c29299ef51991b3e22af022b52005c
Bruno Posa <bruno.posa@gmail.com>
2023-11-27 13:05:54 +0200

    pen and paper version

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,47 @@
 /*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 21:06:37 by bposa             #+#    #+#             */
-/*   Updated: 2023/11/26 23:54:48 by bposa            ###   ########.fr       */
+/*   Updated: 2023/11/27 13:04:13 by bposa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+
+static void	free_all(const char **s, size_t i)
+{
+	while (i > 0)
+	{
+		free((void *)*s[i]);
+		i--;
+	}
+	free((void *)*s[i]);
+	free((void *)*s);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	/*mallocate total amount*/
-	/*search delimiting char*/
-	/*put substring into index of array - keep putting them in loop*/
-	/*free() necessary things*/
-	
-	char	**arr;
+	char	**str;
+	char	*next;
 	size_t	i;
-	size_t	j;
-	char	*temp; // do I need to malloc this?
 
 	i = 0;
-	j = 0;
-	**arr = (char *)malloc(sizeof(s) + 1);
-	while (ft_strchr(&s[i], c) != 0)
+	while (*s != '\0')
 	{
-		temp = ft_strchr(&s[i], c);
-		*arr[j] = ft_substr(&s[i], i, ft_strchr(&s[i + 1], c) - temp); // *arr[0] takes string from s[0] to 1st delimit char,strchr needs 0 check
-		j++;
+		if (ft_strchr(s + 1, c) != 0)
+			next = ft_strchr(s + 1, c);
+		else
+			next = s + ft_strlen(s);
+		*str[i] = ft_substr(s, 0, next - s); 
+		if (!*str[i])
+		{
+			free_all(**str, i);
+			return (0);
+		}
 		i++;
+		s = next;
 	}
-	ft_strlcpy(*arr[j], &s[i], ft_strlen(&s[i]));
-	*arr[j + 1] = 0;
+	*str[i + 1] = 0;
+	return (**str);
 }
