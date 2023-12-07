33d6fbbc799a9bd85c48698c1af60233201fcd72
BrunoPosa <84806733+BrunoPosa@users.noreply.github.com>
2023-12-07 22:37:31 +0200

    Remade ft_atoi.c at end due to wrong overflow check

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,55 +11,29 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-static int	bp_isspace(int c)
+int	ft_atoi(const char *s)
 {
-	char	spaces[7];
+	long long	sign;
+	long long	result;
 
-	spaces[0] = ' ';
-	spaces[1] = '\f';
-	spaces[2] = '\n';
-	spaces[3] = '\r';
-	spaces[4] = '\t';
-	spaces[5] = '\v';
-	if (c == EOF)
-	{
-		return (0);
-	}
-	if (ft_memchr(spaces, c, ft_strlen(spaces)) != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-int	ft_atoi(const char *str)
-{
-	long	num;
-	long	sign;
-
-	num = 0;
 	sign = 1;
-	while (bp_isspace(*str) == 1 && *str != '\0')
-		str++;
-	if (*str == '-' || *str == '+')
+	result = 0;
+	while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+		sign = ',' - *s++;
+	while (*s && (*s >= '0' && *s <= '9'))
 	{
-		sign = ',' - *str;
-		str++;
-	}
-	while (ft_isdigit(*str) == 1 && *str != '\0')
-	{
-		if (num > LONG_MAX / 10 || (num == LONG_MAX / 10 && *str > '7'))
+		if (result > LLONG_MAX / 10
+			|| (result == LLONG_MAX / 10 && *s - '0' > LLONG_MAX % 10))
 		{
 			if (sign == -1)
 				return (0);
 			return (-1);
 		}
-		num = num * 10 + *str - '0';
-		str++;
+		result = result * 10 + *s++ - '0';
 	}
-	return (num * sign);
+	return (sign * result);
 }
